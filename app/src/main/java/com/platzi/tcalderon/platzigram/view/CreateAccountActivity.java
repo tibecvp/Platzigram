472eff2e8cd1e67df776728e318dc4d8c6e6c97e
“tibecvp” <tibecvp@gmail.com>
2017-08-18 17:37:45 -0500

    Initial commit

---
+++ app/src/main/java/com/platzi/tcalderon/platzigram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.platzi.tcalderon.platzigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.platzi.tcalderon.platzigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
