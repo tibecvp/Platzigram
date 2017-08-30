80548127d6aa02679b3f183143fa3dcefcacaba6
tibecvp <tibecvp@gmail.com>
2017-08-29 23:16:41 -0500

    Se termina de desarrollar la vista de Login

--- app/src/main/java/com/platzi/tcalderon/platzigram/LoginActivity.java
+++ app/src/main/java/com/platzi/tcalderon/platzigram/LoginActivity.java
@@ -2,6 +2,7 @@ package com.platzi.tcalderon.platzigram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view){
+
     }
 }
