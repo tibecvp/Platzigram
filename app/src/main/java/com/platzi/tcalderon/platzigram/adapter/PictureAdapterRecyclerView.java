c81a31a2626184fb8161694db45ffa816c9f8f78
“tibecvp” <tibecvp@gmail.com>
2017-09-05 17:03:58 -0500

    Se implementa el bottomActionBar para la clase ContainerActivity con sus diferentes fragments

---
+++ app/src/main/java/com/platzi/tcalderon/platzigram/adapter/PictureAdapterRecyclerView.java
@@ -0,0 +1,20 @@
+package com.platzi.tcalderon.platzigram.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by tcalderon on 9/5/17.
+ */
+
+public class PictureAdapterRecyclerView {
+
+    public class PictureViewHolder extends RecyclerView.ViewHolder {
+
+
+
+        public PictureViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
